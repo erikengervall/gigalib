be7cab21fba1800d93b37718d61b43dc7b363bdf
Erik Engervall <erik.engervall@gmail.com>
2022-01-25 20:36:31 +0100

    add eslint & prettier

--- packages/naiveDeepclone/src/index.ts
+++ packages/naiveDeepclone/src/index.ts
@@ -2,8 +2,5 @@
  * A very naive implementation of deepclone
  */
 export function naiveDeepclone<T>(someObject: T): T {
-  const unusedVariable = "what";
-  console.log("this shouldnt be here!");
-
-  return JSON.parse(JSON.stringify(someObject));
+  return JSON.parse(JSON.stringify(someObject))
 }
