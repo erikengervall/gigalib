5b6c1f538de9a3c1f10b44a7368e1d3fe06136a9
Erik Engervall <erik.engervall@gmail.com>
2024-08-14 22:55:28 +0900

    🚀 ~ packages/mrgen/src/index.ts

--- packages/mrgen/src/index.ts
+++ packages/mrgen/src/index.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import { createSharedFile } from './utils/create-shared-file';
 
 import { MRGenOptions } from './types/mrgen-options';
@@ -14,31 +12,31 @@ export async function MRGen(options: MRGenOptions) {
   createFrontendFile(options);
 }
 
-MRGen({
-  data: {
-    queryParams: true,
-    payload: true,
-    params: true,
-    responseBody: true,
-  },
+// MRGen({
+//   data: {
+//     queryParams: true,
+//     payload: true,
+//     params: true,
+//     responseBody: true,
+//   },
 
-  httpMethod: 'POST',
+//   httpMethod: 'POST',
 
-  shared: {
-    apiDir: path.join(process.cwd(), 'packages/shared/src/api'),
-    file: path.join(process.cwd(), 'packages/shared/src/api/v1/user/get.ts'),
-    buildCommand: 'yarn workspace shared ci:build',
-  },
-  backend: {
-    file: path.join(
-      process.cwd(),
-      'packages/backend/src/handlers/v1/user/get-handler.ts',
-    ),
-  },
-  frontend: {
-    file: path.join(
-      process.cwd(),
-      'packages/frontend1/src/api/hooks/v1-user-get.ts',
-    ),
-  },
-});
+//   shared: {
+//     apiDir: path.join(process.cwd(), 'packages/shared/src/api'),
+//     file: path.join(process.cwd(), 'packages/shared/src/api/v1/user/get.ts'),
+//     buildCommand: 'yarn workspace shared ci:build',
+//   },
+//   backend: {
+//     file: path.join(
+//       process.cwd(),
+//       'packages/backend/src/handlers/v1/user/get-handler.ts',
+//     ),
+//   },
+//   frontend: {
+//     file: path.join(
+//       process.cwd(),
+//       'packages/frontend1/src/api/hooks/v1-user-get.ts',
+//     ),
+//   },
+// });
