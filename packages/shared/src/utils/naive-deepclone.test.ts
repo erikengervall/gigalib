dc7629c670471e5ad4d1be4f1742b65b9e620b9f
Erik Engervall <erik.engervall@gmail.com>
2024-08-14 22:09:35 +0900

    restructure

---
+++ packages/shared/src/utils/naive-deepclone.test.ts
@@ -0,0 +1,44 @@
+import { naiveDeepclone } from './naive-deepclone';
+
+describe('handle mutations within the nestled structure', () => {
+  it('expected outcome if not deepclone', () => {
+    const myObj = {
+      some: {
+        nestled: {
+          value: 1337,
+        },
+      },
+    };
+
+    const naiveClone = { ...myObj };
+    myObj.some.nestled = '1337' as never;
+
+    expect(naiveClone.some).toMatchInlineSnapshot(`
+      Object {
+        "nestled": "1337",
+      }
+    `);
+  });
+
+  it('should deepclone objects, modifying values are not propagated', () => {
+    const myObj = {
+      some: {
+        nestled: {
+          value: 1337,
+        },
+      },
+    };
+
+    const naiveClone = naiveDeepclone(myObj);
+
+    myObj.some = '1337' as never;
+
+    expect(naiveClone.some).toMatchInlineSnapshot(`
+      Object {
+        "nestled": Object {
+          "value": 1337,
+        },
+      }
+    `);
+  });
+});
---
+++ packages/shared/src/utils/naive-deepclone.ts
@@ -0,0 +1,6 @@
+/**
+ * A very naive implementation of deepclone
+ */
+export function naiveDeepclone<T>(someObject: T): T {
+  return JSON.parse(JSON.stringify(someObject));
+}
