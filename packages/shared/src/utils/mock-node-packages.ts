22a9608ff0a7dd2a0e23a3422a4ae77172a85c32
Erik Engervall <erik.engervall@gmail.com>
2024-08-16 20:49:37 +0900

    🚀 ~ packages/shared/src/utils/mock-node-packages.ts

--- packages/shared/src/utils/mock-node-packages.ts
+++ packages/shared/src/utils/mock-node-packages.ts
@@ -1,13 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 export const mockCp = {
+  // @ts-ignore
   execSync: jest.fn(),
 };
 
 export const mockFs = {
+  // @ts-ignore
   mkdirSync: jest.fn(),
+  // @ts-ignore
   writeFileSync: jest.fn(),
+  // @ts-ignore
   readdirSync: jest.fn(() => {
     return ['index.ts', 'module-one.ts', 'module-two.ts', 'module-three.ts'];
   }),
+  // @ts-ignore
   lstatSync: jest.fn(() => {
     return {
       isDirectory: () => false,
@@ -16,10 +23,12 @@ export const mockFs = {
 };
 
 export function mockNodePackages() {
+  // @ts-ignore
   jest.mock('node:child_process', () => {
     return mockCp;
   });
 
+  // @ts-ignore
   jest.mock('node:fs', () => {
     return mockFs;
   });
@@ -27,12 +36,14 @@ export function mockNodePackages() {
 
 export function fsSnapshots() {
   for (const [fsMethod, mockFn] of Object.entries(mockFs)) {
+    // @ts-ignore
     expect(mockFn.mock.calls).toMatchSnapshot(fsMethod);
   }
 }
 
 export function cpSnapshots() {
   for (const [cpMethod, mockFn] of Object.entries(mockCp)) {
+    // @ts-ignore
     expect(mockFn.mock.calls).toMatchSnapshot(cpMethod);
   }
 }
