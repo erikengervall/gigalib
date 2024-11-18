613280db0a389a7c15c3292b2e8becea45e8da61
Erik Engervall <erik.engervall@gmail.com>
2024-11-18 22:31:53 +0900

    Improve upperCaseFirstCharacterAllWords

--- packages/shared/src/utils/upper-case-first-character-all-words.ts
+++ packages/shared/src/utils/upper-case-first-character-all-words.ts
@@ -1,5 +1,5 @@
 import { upperCaseFirstCharacter } from './upper-case-first-character';
 
-export function upperCaseFirstCharacterAllWords(str: string) {
-  return str.split(' ').map(upperCaseFirstCharacter).join(' ');
+export function upperCaseFirstCharacterAllWords(str: string, separator = ' ') {
+  return str.split(separator).map(upperCaseFirstCharacter).join(' ');
 }
